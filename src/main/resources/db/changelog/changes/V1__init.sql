f99ee4652cec897a45e828718b2f9ef71c77524d
Yuri Claro <yuri.claro@outlook.com>
2024-10-23 15:08:50 -0300

    feat: add SQLs files

---
+++ src/main/resources/db/changelog/changes/V1__init.sql
@@ -0,0 +1,5 @@
+CREATE TABLE tb_user_credential (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255),
+    password VARCHAR(255)
+);
---
+++ src/main/resources/db/changelog/changes/V2__insert.data.sql
@@ -0,0 +1,2 @@
+INSERT INTO tb_user_credential (username, password) VALUES
+('user', '1234');
