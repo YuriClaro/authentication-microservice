d0256f387e995f2936a58c46c6a569dbde0a6b30
Yuri Claro <yuri.claro@outlook.com>
2024-10-23 15:04:53 -0300

    feat: add SignInRequest.java

---
+++ src/main/java/com/humanit/authentication_microservice/model/SignInRequest.java
@@ -0,0 +1,19 @@
+package com.humanit.authentication_microservice.model;
+
+public class SignInRequest {
+    private String username;
+    private String password;
+
+    public SignInRequest(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
